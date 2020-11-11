398675813c01c683f061b61d27bc825edf3cfb9e
vladimir.l <uYm2j95pXbcad>
2020-11-11 19:17:07 +0200

    Add classes examples

--- hello-world.py
+++ hello-world.py
@@ -11,8 +11,22 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def print_name(self):
+        print("Hallo. Ich heisse " + self.name )
         
-person1 = Person('John', 66)
+    def print_age(self):
+        print("Ich bin " + str(self.age) + " Jahre alt")
+        
+person1 = Person('Ben', 36)
 
 print( person1.name )
 print( person1.age )
+
+person1.print_name();
+person1.print_age();
+
+class PersonSuper:
+    pass
+    
+    
